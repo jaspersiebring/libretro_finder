259e39d3305c76d51be23ecf4ffaa27b6d31cd4d
Jasper <j.siebring92@gmail.com>
2023-07-05 17:24:08 +0200

    Moved some of the more generic functions to utils.py

---
+++ utils.py
@@ -0,0 +1,20 @@
+import pathlib
+import hashlib
+import concurrent.futures
+from typing import Tuple, List
+from config import MAX_BIOS_BYTES
+    
+def hash_file(file_path: pathlib.Path) -> str:
+    bytes = file_path.read_bytes()
+    hash = hashlib.md5(bytes)
+    return hash.hexdigest()
+
+def recursive_hash(directory: pathlib.Path, glob: str = "*") -> Tuple[List[pathlib.Path], List[str]]:
+    file_paths = list(directory.rglob(pattern=glob))
+    file_paths = [file_path for file_path in file_paths if file_path.stat().st_size <= MAX_BIOS_BYTES and file_path.is_file()]
+
+    file_hashes = []            
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for hash in executor.map(hash_file, file_paths):
+            file_hashes.append(hash)
+    return file_paths, file_hashes
